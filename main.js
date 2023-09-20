42eac1adc7917a93578caf3cb06a0ec458f293e5
Kris <kharonek@yahoo.com>
2023-09-19 22:01:34 -0300

    add javascript
    added book object that will be used to store book data in the library array

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log("library works")
+
+const myLibrary = [];
+
+function Book(title, author, read=false) {
+    this.title = title;
+    this.author = author;
+    this.read = read;
+}
+
+function addBookToLibrary() {
+    return 0;
+}
+
