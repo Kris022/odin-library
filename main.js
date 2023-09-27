6182d15cb8885139736755012dd006f204472214
Kris <kharonek@yahoo.com>
2023-09-27 16:43:43 -0300

    handle form submits
    create event listener for book form submit which allows adding new books to the library

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
-
 const myLibrary = [];
 
-function Book(title, author, read=false) {
-    this.title = title;
-    this.author = author;
-    this.read = read;
+function Book(title, author, read = false) {
+  this.title = title;
+  this.author = author;
+  this.read = read;
 }
 
-function addBookToLibrary() {
-    return 0;
+//
+function addBookToLibrary(newBook) {
+  myLibrary.push(newBook);
+  return 0;
 }
 
+// Get input from the add book form
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const titleField = document.getElementById("title");
+  const authorField = document.getElementById("author");
+  const readField = document.getElementById("read");
+
+  console.log("works");
+});
