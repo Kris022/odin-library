914dc3ec3dfb816edc44546966851ffcc722151e
Kris <kharonek@yahoo.com>
2023-09-27 23:20:30 -0300

    completed basic functionality
    ability to add books

--- main.js
+++ main.js
@@ -11,8 +11,30 @@ function addBookToLibrary(newBook) {
   myLibrary.push(newBook);
 }
 
+const libraryDisplayElement = document.querySelector(".book-grid-wrapper");
+
 function displayBooks() {
-    return false;
+  libraryDisplayElement.innerHTML = "";
+
+  // Find book thats not
+  for (let i = 0; i < myLibrary.length; i++) {
+    const book = myLibrary[i];
+
+    const div = document.createElement("div");
+    div.classList.add("book-card");
+    const h1 = document.createElement("h3");
+    h1.textContent = book.title;
+    const h3 = document.createElement("h4");
+    h3.textContent = book.author;
+    const h6 = document.createElement("h6");
+    h6.textContent = book.read ? "Read" : "Not Read";
+
+    div.appendChild(h1);
+    div.appendChild(h3);
+    div.appendChild(h6);
+
+    libraryDisplayElement.appendChild(div);
+  }
 }
 
 // Get input from the add book form
@@ -23,11 +45,15 @@ form.addEventListener("submit", (event) => {
 
   const titleField = document.getElementById("title").value;
   const authorField = document.getElementById("author").value;
-  const readField = document.getElementById("read").value;
+  const readField = document.getElementById("read").checked;
 
   addBookToLibrary(new Book(titleField, authorField, readField));
   console.log(myLibrary);
 
   // Reset the input fields
-
+  displayBooks();
+  form.reset();
 });
+
+addBookToLibrary(new Book("book title", "book author", false));
+displayBooks();
