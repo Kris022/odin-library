41c486920e0a17f70ff7c9fe8f334100821c8325
Kris <kharonek@yahoo.com>
2023-09-28 21:16:54 -0300

    display book function

--- main.js
+++ main.js
@@ -1,6 +1,43 @@
 const showButton = document.querySelector("#showDialog");
 const formDialog = document.querySelector("#formDialog");
 
+const gridContainer = document.querySelector(".grid-container");
+
 showButton.addEventListener("click", () => {
   formDialog.showModal();
 });
+
+const myLibrary = [];
+
+function Book(title, author, readStatus) {
+  this.title = title;
+  this.author = author;
+  this.readStatus = readStatus;
+}
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
+
+function resetBookDisplay() {
+  gridContainer.innerHTML = "";
+}
+
+function displayBooks() {
+  for (let i = 0; i < myLibrary.length; i++) {
+    const book = myLibrary[i];
+
+    const bookCard = `
+    <div class="book-card">
+      <p>${book.author}</p>
+      <h2>${book.title}</h2>
+      <div class="book-status">${book.readStatus}</div>
+    </div>
+    `;
+
+    gridContainer.appendChild(bookCard);
+  }
+}
+
+addBookToLibrary(new Book("some title", "some author", "not started"));
+console.log(myLibrary);
